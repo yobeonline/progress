6dffa8cbfb947990229621c7047fda2edf29972b
Yohann Bénédic <yohann.benedic@gmail.com>
2022-05-10 17:08:52 +0200

    Changed task to accept report functions. Added a test case for range usage.

--- include/task.hpp
+++ include/task.hpp
@@ -1,16 +1,37 @@
 #pragma once
 #include <iostream>
+#include <functional>
 
 namespace io1::progress{
+
+  struct report_functions
+  {
+    std::function<void()> start{ [] {} };
+    std::function<void(float)> progress{ [](auto) {} };
+    std::function<void(bool)> finish{ [](auto) {} };
+  };
+
   class task
   {
   public:
-    void start(size_t target) noexcept { target_ = target; progress_ = 0; std::cout << "start\n"; };
-    ~task() noexcept { if (success()) std::cout << "success\n"; else std::cout << "failed\n"; };
+    task() = default;
+    task(task const&) = delete;
+    task(task&&) = default;
+    task& operator=(task const&) = delete;
+    task& operator=(task&&) = default;
+    ~task() noexcept
+    {
+      report_.finish(success());
+    };
+
+    explicit task(report_functions report) noexcept :report_(std::move(report)) {};
+
+    void start(size_t target) noexcept { target_ = target; progress_ = 0; report_.start(); };
+
     task& operator++()
     {
       ++progress_;
-      std::cout << (100.f * progress_) / target_ <<"%\n";
+      report_.progress((100.f * progress_) / target_);
       return *this;
     };
     [[nodiscard]] bool success() const noexcept { return target_ <= progress_; };
@@ -18,7 +39,12 @@ namespace io1::progress{
   private:
     size_t target_;
     size_t progress_;
+    report_functions report_;
   };
+
+
+
+
 
   template<typename ITERATOR>
   class task_iterator_wrapper
@@ -62,3 +88,92 @@ namespace io1::progress{
     return task_view<RANGE>(r, t);
   };
 }
+/*
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ranges>
+#include <concepts>
+#include <algorithm>
+#include <assert.h>
+
+namespace rg = std::ranges;
+
+template<rg::input_range R> requires rg::view<R>
+class custom_take_view : public rg::view_interface<custom_take_view<R>>
+{
+private:
+    R                                         base_ {};
+    std::iter_difference_t<rg::iterator_t<R>> count_ {};
+    rg::iterator_t<R>                         iter_ {std::begin(base_)};
+public:
+    custom_take_view() = default;
+
+    constexpr custom_take_view(R base, std::iter_difference_t<rg::iterator_t<R>> count)
+        : base_(base)
+        , count_(count)
+        , iter_(std::begin(base_))
+    {}
+
+    constexpr R base() const &
+    {return base_;}
+    constexpr R base() &&
+    {return std::move(base_);}
+
+    constexpr auto begin() const
+    {return iter_;}
+    constexpr auto end() const
+    { return std::next(iter_, count_); }
+
+    constexpr auto size() const requires rg::sized_range<const R>
+    {
+        const auto s = rg::size(base_);
+        const auto c = static_cast<decltype(s)>(count_);
+        return s < c ? 0 : s - c;
+    }
+};
+
+template<class R>
+custom_take_view(R&& base, std::iter_difference_t<rg::iterator_t<R>>)
+    -> custom_take_view<rg::views::all_t<R>>;
+
+namespace details
+{
+    struct custom_take_range_adaptor_closure
+    {
+        std::size_t count_;
+        constexpr custom_take_range_adaptor_closure(std::size_t count): count_(count)
+        {}
+
+        template <rg::viewable_range R>
+        constexpr auto operator()(R && r) const
+        {
+            return custom_take_view(std::forward<R>(r), count_);
+        }
+    } ;
+
+    struct custom_take_range_adaptor
+    {
+        template<rg::viewable_range R>
+        constexpr auto operator () (R && r, std::iter_difference_t<rg::iterator_t<R>> count)
+        {
+            return custom_take_view( std::forward<R>(r), count ) ;
+        }
+
+        constexpr auto operator () (std::size_t count)
+        {
+            return custom_take_range_adaptor_closure(count);
+        }
+    };
+
+    template <rg::viewable_range R>
+    constexpr auto operator | (R&& r, custom_take_range_adaptor_closure const & a)
+    {
+        return a(std::forward<R>(r)) ;
+    }
+}
+
+namespace views
+{
+    details::custom_take_range_adaptor custom_take;
+}*/
--- test/test_task.cpp
+++ test/test_task.cpp
@@ -4,12 +4,48 @@
 #define BOOST_TEST_MODULE io1::progress test
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(range)
+BOOST_AUTO_TEST_CASE(range_semantic)
 {
-  io1::progress::task t;
-  std::vector<int> vec = { 1, 2, 3, 4, 5 };
+  size_t index = 0;
+  bool started = false;
+  bool finished = false;
 
-  for (auto const & v : vec | t) std::cout << "parsing " << v << '\n';
+  std::vector<int> const vec = { 1, 2, 3, 4, 5 };
 
-  // should display "start, 20%, 40%, 60%, 80%, 100%, sucess
+  auto const start = [&index, &started, &finished]
+  {
+    BOOST_CHECK_EQUAL(0, index);
+    BOOST_CHECK(!started);
+    BOOST_CHECK(!finished);
+    started = true;
+  };
+
+  auto const report = [&index, &started, &finished](float progress)
+  {
+    constexpr float expected[] = { 20.f, 40.f, 60.f, 80.f, 100.f };
+    BOOST_CHECK_EQUAL(progress, expected[index]);
+    BOOST_CHECK(started);
+    BOOST_CHECK(!finished);
+    ++index;
+  };
+
+  auto const finish = [&index, &started, &finished, &vec](bool success)
+  {
+    BOOST_CHECK_EQUAL(vec.size(), index);
+    BOOST_CHECK(success);
+    BOOST_CHECK(started);
+    BOOST_CHECK(!finished);
+    finished = true;
+  };
+
+  {
+    io1::progress::task t({start, report, finish});
+    size_t i = 0;
+    for (auto const& v : vec | t) BOOST_CHECK_EQUAL(v, vec[i++]);
+  }
+
+  BOOST_CHECK(started);
+  BOOST_CHECK(finished);
+
+  return;
 }
